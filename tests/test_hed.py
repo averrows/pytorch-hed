68cf867df9dfc1cefaed08e50bcdac860d1821b5
davidelanz <lanza.davide.it@gmail.com>
2020-12-16 10:50:07 +0000

    Updated docs

--- tests/test_hed.py
+++ tests/test_hed.py
@@ -5,5 +5,5 @@ def test_hed_on_sample():
     torchHED.process_img("./images/sample.png", "./images/torchHED.png")
 
 
-def test_hed_on_different_imgs():
+def test_hed_on_folder():
     torchHED.process_folder("./tests/test_imgs", "./tests/tests_imgs_out")
