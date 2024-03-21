acc6e1f5a3189f24d6eb48803fb4d22cc3c7b156
Averrous Saloom <averous009@gmail.com>
2024-03-21 13:23:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url=about['__url__'],
     packages=['torchHED'],
     include_package_data=True,
-    python_requires=">=3.7.*",
+    python_requires=">=3.7",
     license=about['__license__'],
     zip_safe=False,
     entry_points={
